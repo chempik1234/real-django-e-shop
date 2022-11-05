85950a145e4fe4d44555cf5a5f24d109497cccec
chempik1234 <dandijar@yandex.ru>
2022-11-05 11:37:14 +0500

    first after successful pull

--- usadba_project/usadba_project/urls.py
+++ usadba_project/usadba_project/urls.py
@@ -36,6 +36,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
-# if bool(settings.DEBUG):
-#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if bool(settings.DEBUG):
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
