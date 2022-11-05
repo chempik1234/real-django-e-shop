d83199a455d79f2b818a060e3e0a0b30d55dd832
chempik1234 <dandijar@yandex.ru>
2022-11-05 12:20:21 +0500

    static indian tutorial

--- usadba_project/usadba_project/settings.py
+++ usadba_project/usadba_project/settings.py
@@ -126,11 +126,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
-STATIC_ROOT = '/home/imfighter160/usadba_project/usadba_app/static/'
+# STATIC_ROOT = '/home/imfighter160/usadba_project/usadba_app/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
