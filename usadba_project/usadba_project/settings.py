84c9e736f5b5781c232f4f58c5f39019a3b453fb
chempik1234 <dandijar@yandex.ru>
2022-11-05 11:42:49 +0500

    first after successful pull

--- usadba_project/usadba_project/settings.py
+++ usadba_project/usadba_project/settings.py
@@ -130,7 +130,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = '/home/imfighter160/usadba_project/usadba_app/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
