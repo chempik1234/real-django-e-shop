082fd49216703aa5024d9897921a8e5373ee8dd2
chempik1234 <dandijar@yandex.ru>
2022-11-11 17:33:23 +0500

    ummmmm

--- usadba_project/git_pull/views.py
+++ usadba_project/git_pull/views.py
@@ -7,7 +7,7 @@ import git
 @csrf_exempt
 def github_update_pythonanywhere(request):
     if request.method == 'POST':
-        repo = git.Repo('./', search_parent_directories=True)
+        repo = git.Repo('./usadba_project', search_parent_directories=True)
         _git = repo.git
         _git.checkout('master')
         _git.pull()
